b4d68e5023074d5edac6c55b18546b6de5a2f262
surendra <surendra100598@gmail.com>
2019-10-02 12:05:10 +0530

    low and high temp

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (lt, lg, callback) => {
         } else if (body.error) {
             callback('Unable to find location. Try another search.', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. ' + "Today's high temperature is " + body.daily.data[0].temperatureHigh + ' degrees and low temperature is ' + body.daily.data[0].temperatureLow + ' degrees. There is ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
